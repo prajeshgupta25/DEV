b8e3b6133c14870d6bbc30edef294b3115c619b1
Prajesh Gupta <imprj.gupta@gmail.com>
2021-06-12 02:43:32 +0530

    async practice

--- Async/nFiles/asyncParallel.js
+++ Async/nFiles/asyncParallel.js
@@ -1,2 +1,19 @@
 let fs = require("fs");
 let files = ["../f1.txt" , "../f2.txt" , "../f3.txt"];
+
+function callme(start){
+  if(start==files.length){
+    return;
+  }
+
+  fs.readFile(files[start] , cb);
+
+  function cb(error , data){
+      // console.log(error);
+      console.log(data+"");
+  }
+
+  callme(start+1);
+}
+
+callme(0);
