8c69895195c0516a2cbbd9638bafc7d3453ee8d8
Prajesh Gupta <imprj.gupta@gmail.com>
2021-06-15 13:00:33 +0530

    all subsidery changes

--- Async/nFiles/asyncParallel.js
+++ Async/nFiles/asyncParallel.js
@@ -1,19 +1,30 @@
 let fs = require("fs");
 let files = ["../f1.txt" , "../f2.txt" , "../f3.txt"];
 
-function callme(start){
-  if(start==files.length){
-    return;
-  }
 
-  fs.readFile(files[start] , cb);
+//Recursion
 
-  function cb(error , data){
-      // console.log(error);
-      console.log(data+"");
-  }
+// function callme(start){
+//   if(start==files.length){
+//     return;
+//   }
 
-  callme(start+1);
+//   fs.readFile(files[start] , cb);
+
+//   function cb(error , data){
+//       // console.log(error);
+//       console.log(data+"");
+//   }
+
+//   callme(start+1);
+// }
+
+// callme(0);
+
+//Looping
+
+for(let i=0 ; i<files.length ; i++){
+    fs.readFile(files[i] , function(err , data){
+        console.log(data+"");
+    })
 }
-
-callme(0);
